50bbb7346a942e23da0a7f936831ee5437c634ee
Alvaro Garcia <alvarogarcia0001@gmail.com>
2019-05-16 16:25:14 -0500

    Added taco bells parser

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -12,18 +12,41 @@ namespace LoggingKata.Test
         }
 
         [Theory]
-        [InlineData("Example")]
+        [InlineData("34.376395,-84.913185,Taco Bell Adairsvill")]
         public void ShouldParse(string str)
         {
-            // TODO: Complete Should Parse
+            //Arrange
+            TacoParser tacoParser = new TacoParser();
+
+            // Act
+            ITrackable actual = tacoParser.Parse(str);
+
+            // Assert
+            Assert.NotNull(actual);
         }
 
         [Theory]
-        [InlineData(null)]
-        [InlineData("")]
+        [InlineData("0,abc,Tacobell")]
+        [InlineData("abc,0,Tacobell")]
+        [InlineData("abc,abc,Tacobell")]
+        [InlineData("300,0,Tacobell")]
+        [InlineData("90.00001,84,Tacobell")]
+        [InlineData("84,180.000001,Tacobell")]
+        [InlineData(",0,Tacobell")]
+        [InlineData("0,0,")]
+        [InlineData("34.376395")]
+        [InlineData("34.376395,-84.913185")]
         public void ShouldFailParse(string str)
         {
-            // TODO: Complete Should Fail Parse
+            // Arrange
+            TacoParser tacoParser = new TacoParser();
+
+            //Act   
+            ITrackable actual = tacoParser.Parse(str);
+
+            //Assert
+            Assert.Null(actual);
+
         }
     }
 }
